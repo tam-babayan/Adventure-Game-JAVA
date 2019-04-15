41dc377914fdf891b1ec10e99fb3532706f21830
tam-babayan <tam.babayan@gmail.com>
2019-04-15 12:13:55 +0200

    added final keyword to Location class

--- src/com/tbabayan/Location.java
+++ src/com/tbabayan/Location.java
@@ -3,7 +3,7 @@ package com.tbabayan;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Location {
+public final class Location {
 
     private final int locationID;
     private final String description;
@@ -14,7 +14,7 @@ public class Location {
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = exits;
+        this.exits = new HashMap<>(exits);
         this.exits.put("Q", 0);
     }
 
